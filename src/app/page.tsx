cc712d33d2d1e756f282462db668dfb7ef6716db
dd <dd5892631@gmail.com>
2024-09-10 13:46:35 +0300

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>111 and see your changes instantly.</li>
+          <li>1111 and see your changes instantly.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
